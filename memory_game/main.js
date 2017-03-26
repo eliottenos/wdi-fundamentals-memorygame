ae8686e2451a70ef08b5a4f63acad5080cbc0268
eliottenos <eliottenos@gmail.com>
2017-03-26 16:16:47 -0400

    Add files via upload

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,7 +1,39 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("JS file is connected to HTML! Woo!")
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = {'queen', 'queen', 'king', 'king'};
+var cardsInPlay = {};
+var board = document.getElementById('game-board');
+
+function createBoard() {
+	for (i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards {i});
+		cardElement.addEvenListener('click', isTwoCards);
+		board.appendChild(cardElement);
+	}
+}
+
+function isTwoCards() {
+	cardsInPlay.push (this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king'){
+        this.innerHTML = "<img src='my-king.png' alt ='King of Hearts' />";
+        else {
+        	this.innerHTML = "<img src='my-queen.png' alt='Queen of Hearts' />";
+        }
+	} if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = {};
+		this.innerHTML = " ";
+	}
+}
+
+function isMatch(cards) {
+	if (cards{0} === cards{1}) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+createBoard();
